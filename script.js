15c68acbfe3cb751cdedeb9b6371f8c387d0b97e
KatieQuach <2021097@icacademy.org>
2019-10-01 01:28:53 +0000

    insert comment

---
+++ script.js
@@ -0,0 +1,10 @@
+$( "document" ).ready(function() {
+
+
+$("#button").click(function() {
+let input = $(".input").val();
+$(".output").text();
+}
+//take the word out of the text box and display it back on the screen
+
+});
